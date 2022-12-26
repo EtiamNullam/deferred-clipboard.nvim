500329fa3a6946bffb53782a38cfb77d6190f6e0
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2022-12-27 00:34:25 +0200

    feat: Disable continuous clipboard sync on first focus change event

--- lua/deferred-clipboard.lua
+++ lua/deferred-clipboard.lua
@@ -1,6 +1,24 @@
 local M = {}
 
-M.setup = function()
+local function is_continuous_clipboard_sync_enabled()
+    return vim.o.clipboard ~= nil
+        and vim.o.clipboard ~= ''
+end
+
+local function schedule_disable_of_continuous_clipboard_sync_on_focus_change()
+    vim.api.nvim_create_autocmd({
+        'FocusGained',
+        'FocusLost',
+    }, {
+        once = true,
+        pattern = '*',
+        callback = function()
+            vim.o.clipboard = nil
+        end,
+    })
+end
+
+local function schedule_clipboard_sync_on_focus_change()
     local deferred_clipboard_sync_group = vim.api.nvim_create_augroup(
         'DeferredClipboardSync',
         { clear = true }
@@ -13,29 +31,32 @@ M.setup = function()
         )
     end
 
-    vim.api.nvim_create_autocmd(
-        {
-            'FocusLost',
-            'VimLeavePre'
-        }, {
-            group = deferred_clipboard_sync_group,
-            pattern = '*',
-            callback = function()
-                copy_register('"', '+')
-            end
-        }
-    )
+    vim.api.nvim_create_autocmd({
+        'FocusLost',
+        'VimLeavePre'
+    }, {
+        group = deferred_clipboard_sync_group,
+        pattern = '*',
+        callback = function()
+            copy_register('"', '+')
+        end
+    })
 
-    vim.api.nvim_create_autocmd(
-        'FocusGained',
-        {
-            group = deferred_clipboard_sync_group,
-            pattern = '*',
-            callback = function()
-                copy_register('+', '"')
-            end
-        }
-    )
+    vim.api.nvim_create_autocmd('FocusGained', {
+        group = deferred_clipboard_sync_group,
+        pattern = '*',
+        callback = function()
+            copy_register('+', '"')
+        end
+    })
+end
+
+function M.setup()
+    schedule_clipboard_sync_on_focus_change()
+
+    if is_continuous_clipboard_sync_enabled() then
+        schedule_disable_of_continuous_clipboard_sync_on_focus_change()
+    end
 end
 
 return M
